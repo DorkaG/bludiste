4cb66301ddd571cd729f688621a20673d2317f50
Xende-W <64082180+Xende-W@users.noreply.github.com>
2020-06-20 17:21:55 +0200

    porovnani kostky

--- src/components/Kostka.vue
+++ src/components/Kostka.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
-      <img  v-bind:src="require(`./../assets/images/1hruska.jpg`)" alt="hruska">
-      <img  v-bind:src="require(`./../assets/images/2mrkev.jpg`)" alt="mrkev">
-      <img  v-bind:src="require(`./../assets/images/3meloun.jpg`)" alt="meloun">
-      <img  v-bind:src="require(`./../assets/images/4ananas.jpg`)" alt="ananas">
-      <img  v-bind:src="require(`./../assets/images/5kokos.jpg`)" alt="kokos">
-      <img  v-bind:src="require(`./../assets/images/6jahoda.jpg`)" alt="jahoda">
+      <img class="ovoce" v-for="(polozka, index) in obrazky"
+                        v-bind:key="index"
+                        v-bind:src="polozka.obrazek"
+                        v-on:click="vyhodnot(index)"
+                        >
 
     <p>{{hod}}</p>
     <button v-on:click="hodKostkou()">HOĎ KOSTKOU</button>
@@ -19,17 +18,37 @@ export default {
     data() {
         return {
             hod: 0,
-
+            obrazky: [
+                {obrazek: require(`./../assets/images/1hruska.jpg`), hodnota: 1},
+                {obrazek: require(`./../assets/images/2mrkev.jpg`), hodnota: 2},
+                {obrazek: require(`./../assets/images/3meloun.jpg`), hodnota: 3},
+                {obrazek: require(`./../assets/images/4ananas.jpg`), hodnota: 4},
+                {obrazek: require(`./../assets/images/5kokos.jpg`), hodnota: 5},
+                {obrazek: require(`./../assets/images/6jahoda.jpg`), hodnota: 6},
+            ]
         }
     },
 
     methods: {
         hodKostkou(){
-            this.hod = Math.floor(Math.random() * 6 + 1);
-        }
+            this.hod = Math.floor(Math.random() * 6 + 0.5);
+        },
 
+        vyhodnot(index) {                              
+            if (this.hod === this.obrazky[index].hodnota) {
+                this.$emit("vyhrani");    
+                console.log("vyhra")           
+            }
+
+            else {
+                this.$emit("prohrani");
+                console.log("prohra") 
+            }
+        },
 
     }
+
+    
 
 }
 </script>

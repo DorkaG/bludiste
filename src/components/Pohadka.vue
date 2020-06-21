4d286a5d1353f2325f09d5268f6ac47d407ba761
Xende-W <64082180+Xende-W@users.noreply.github.com>
2020-06-21 09:52:07 +0200

    pohádka

--- src/components/Pohadka.vue
+++ src/components/Pohadka.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
+      <h3>Vítej na planetě Kokosmánie!</h3>
+        <p>
+        Pozor! Při přistávání to bude trochu skákat, protože tato planeta má trochu nestabilní i když chutné jádro. 
+        Obyvateli této planety jsou Kokosky. Přátelská stvoření a je s nimi vždy legrace, pokud si ovšem neděláte legraci z kokosů… Tak opatrně.
+        V chrámu z kokosových vláken je socha prastarého Kokosky, který drží posvátný Kokos. Tam by tvé kroky měly směřovat jako první. Vzdát úctu a hold této posvátné plodině. 
+        Na hostině, kterou ti Kokosky připraví, se najíš určitě dosyta. A až ti bude lézt kokos i ušima, spláchni ho dobře vychlazeným kokosovým mlékem. Není nic více osvěžujícího.
+        Při loučení s Kokoskami, obdržíš spoustu darů, pochopitelně kokosových. Kokosový klobouk nos hrdě, alespoň, než planetu opustíš. 
+        Přeji příjemný průzkum.
+        </p>
 
-      
   </div>
 </template>
 
